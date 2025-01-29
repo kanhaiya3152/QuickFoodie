bbf79147c9d631eae7c02412d3a2757ad43dec36
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-01-29 23:40:03 +0530

    add forgot password screen

---
+++ lib/screen/forgot_password_screen.dart
@@ -0,0 +1,180 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/screen/signup_screen.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  String email = "";
+
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> resetPassword() async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(
+            "Email has been sent!",
+            style: TextStyle(color: Colors.white),
+          )));
+    } on FirebaseException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.red,
+            content: Text(
+              "No user found!",
+              style: TextStyle(color: Colors.white),
+            )));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              child: const Text(
+                'Password Recovery',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Enter your email',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Poppins',
+                  fontSize: 20),
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Expanded(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: ListView(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white38, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextFormField(
+                          controller: _emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Enter your email";
+                            }
+                            return null;
+                          },
+                          // style:const TextStyle(color: Colors.white),
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                            hintStyle: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontFamily: 'Poppins'),
+                            prefixIcon: Icon(
+                              Icons.mail_outline,
+                              color: Colors.white70,
+                              size: 30,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() {
+                              email = _emailController.text;
+                            });
+                            resetPassword();
+                          }
+                        },
+                        child: Container(
+                          width: 120,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              color: Colors.redAccent,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: const Center(
+                              child: Text(
+                            "Send mail",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don,t have an account ? ',
+                            style: TextStyle(
+                                color: Colors.white, fontFamily: "Poppins"),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (ctx) => const SignupScreen(),
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                'Create',
+                                style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontFamily: "Poppins"),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
