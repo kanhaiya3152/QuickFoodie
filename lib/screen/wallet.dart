da1204d12459a772253c96437f04fe936aaaac69
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-01-31 02:20:33 +0530

    add navigator

--- lib/screen/wallet.dart
+++ lib/screen/wallet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/screen/payment_screen.dart';
 
 class Wallet extends StatefulWidget {
   const Wallet({super.key});
@@ -10,6 +11,186 @@ class Wallet extends StatefulWidget {
 class _WalletState extends State<Wallet> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Material(
+              elevation: 2,
+              child: Container(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: const Center(
+                  child: Text(
+                    'Wallet',
+                    style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(color: Color(0xFFF2F2F2)),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/wallet.png',
+                    height: 60,
+                    width: 60,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Your Wallet",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "\$" + "100",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Add money",
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFE9E2E2)),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    "\$" + "100",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFE9E2E2)),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    "\$" + "500",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFE9E2E2)),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    "\$" + "1000",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFE9E2E2)),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    "\$" + "2000",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => PaymentScreen(),
+                  ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF008080),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Center(
+                  child: Text(
+                    'Add Money',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
