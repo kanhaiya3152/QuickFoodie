0182e774e6fba830ada9cadceeca9cf5cd5d6310
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-02-15 12:43:06 +0530

    add loader

--- lib/admin/admin_screen.dart
+++ lib/admin/admin_screen.dart
@@ -11,6 +11,7 @@ class AdminScreen extends StatefulWidget {
 
 class _AdminScreenState extends State<AdminScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -25,14 +26,14 @@ class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFededeb),
+      backgroundColor: const Color(0xFFededeb),
       body: Container(
         child: Stack(
           children: [
             Container(
               margin:
                   EdgeInsets.only(top: MediaQuery.of(context).size.height / 2),
-              padding: EdgeInsets.only(top: 45, left: 20, right: 20),
+              padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               decoration: BoxDecoration(
@@ -50,12 +51,12 @@ class _AdminScreenState extends State<AdminScreen> {
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 30, right: 30, top: 60),
+              margin: const EdgeInsets.only(left: 30, right: 30, top: 60),
               child: Form(
                   key: _formKey,
                   child: Column(
                     children: [
-                      Text(
+                      const Text(
                         "Let's start with\n       Admin!",
                         style: TextStyle(
                             color: Colors.black,
@@ -63,7 +64,7 @@ class _AdminScreenState extends State<AdminScreen> {
                             fontWeight: FontWeight.bold,
                             fontFamily: "Poppins"),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 30,
                       ),
                       Material(
@@ -81,12 +82,14 @@ class _AdminScreenState extends State<AdminScreen> {
                                 height: 50,
                               ),
                               Container(
-                                padding: EdgeInsets.only(
+                                padding: const EdgeInsets.only(
                                     left: 20, top: 5, bottom: 5),
-                                margin: EdgeInsets.symmetric(horizontal: 20),
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 20),
                                 decoration: BoxDecoration(
                                     border: Border.all(
-                                      color: Color.fromARGB(255, 160, 160, 147),
+                                      color: const Color.fromARGB(
+                                          255, 160, 160, 147),
                                     ),
                                     borderRadius: BorderRadius.circular(10)),
                                 child: Center(
@@ -98,13 +101,13 @@ class _AdminScreenState extends State<AdminScreen> {
                                       }
                                       return null;
                                     },
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       border: InputBorder.none,
                                       hintText: "Username",
-                                      hintStyle: TextStyle(color:Color.fromARGB(255, 160, 160, 147) ),
-                                    
+                                      hintStyle: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 160, 160, 147)),
                                     ),
-                                    
                                   ),
                                 ),
                               ),
@@ -112,12 +115,14 @@ class _AdminScreenState extends State<AdminScreen> {
                                 height: 40,
                               ),
                               Container(
-                                padding: EdgeInsets.only(
+                                padding: const EdgeInsets.only(
                                     left: 20, top: 5, bottom: 5),
-                                margin: EdgeInsets.symmetric(horizontal: 20),
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 20),
                                 decoration: BoxDecoration(
                                     border: Border.all(
-                                      color: Color.fromARGB(255, 160, 160, 147),
+                                      color: const Color.fromARGB(
+                                          255, 160, 160, 147),
                                     ),
                                     borderRadius: BorderRadius.circular(10)),
                                 child: Center(
@@ -129,13 +134,13 @@ class _AdminScreenState extends State<AdminScreen> {
                                       }
                                       return null;
                                     },
-                                    decoration:const InputDecoration(
+                                    decoration: const InputDecoration(
                                       border: InputBorder.none,
                                       hintText: "Password",
-                                      hintStyle: TextStyle(color:Color.fromARGB(255, 160, 160, 147) ),
-                                    
+                                      hintStyle: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 160, 160, 147)),
                                     ),
-                                    
                                   ),
                                 ),
                               ),
@@ -143,24 +148,33 @@ class _AdminScreenState extends State<AdminScreen> {
                                 height: 60.0,
                               ),
                               GestureDetector(
-                                onTap: (){
+                                onTap: () {
                                   LoginAdmin();
                                 },
                                 child: Container(
-                                  padding:const EdgeInsets.symmetric(vertical: 12.0),
-                                  margin:const EdgeInsets.symmetric(horizontal: 20.0),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 12.0),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20.0),
                                   width: MediaQuery.of(context).size.width,
                                   decoration: BoxDecoration(
-                                      color: Color.fromARGB(255, 30, 29, 29),
+                                      color:
+                                          const Color.fromARGB(255, 30, 29, 29),
                                       borderRadius: BorderRadius.circular(10)),
-                                  child:const Center(
-                                    child: Text(
-                                      "Login",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 20.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
+                                  child: Center(
+                                    child: _isLoading
+                                        ? const Center(
+                                            child: CircularProgressIndicator(
+                                              color: Colors.white,
+                                            ),
+                                          )
+                                        : const Text(
+                                            "Login",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold),
+                                          ),
                                   ),
                                 ),
                               ),
@@ -176,11 +190,15 @@ class _AdminScreenState extends State<AdminScreen> {
       ),
     );
   }
+
   LoginAdmin() {
+    setState(() {
+      _isLoading = true;
+    });
     FirebaseFirestore.instance.collection("admin").get().then((snapshot) {
       snapshot.docs.forEach((result) {
         if (result.data()['id'] != _usernameController.text.trim()) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
               backgroundColor: Colors.orangeAccent,
               content: Text(
                 "Your id is not correct",
@@ -188,16 +206,20 @@ class _AdminScreenState extends State<AdminScreen> {
               )));
         } else if (result.data()['password'] !=
             _passwordController.text.trim()) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
               backgroundColor: Colors.orangeAccent,
               content: Text(
                 "Your password is not correct",
                 style: TextStyle(fontSize: 18.0),
               )));
         } else {
-          Route route = MaterialPageRoute(builder: (context) =>AdminHome());
+          Route route =
+              MaterialPageRoute(builder: (context) => const AdminHome());
           Navigator.pushReplacement(context, route);
         }
+        setState(() {
+          _isLoading = false;
+        });
       });
     });
   }
